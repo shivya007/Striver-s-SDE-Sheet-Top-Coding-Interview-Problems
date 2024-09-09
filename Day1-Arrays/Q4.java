64166a7437d67922219a4d340ecc58ef9e1d89f1
Shiv Arora <passionshiv007@gmail.com>
2024-09-09 22:25:21 +0530

    Maximum Subarray

---
+++ Day1-Arrays/Q4.java
@@ -0,0 +1,13 @@
+// 53. Maximum Subarray -- Kadane's Algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i =0; i < nums.length; i++){
+            currSum += nums[i];
+            maxSum = Math.max(maxSum, currSum);
+            if(currSum < 0) currSum =0;
+        }
+        return maxSum;
+    }
+}
